08b24c9efa90fd06163085425f058a17bc21688f
Mark <m.a.hankinson@gmail.com>
2020-09-21 07:10:11 -0400

    git ignore update

--- secretstuff.py
+++ secretstuff.py
@@ -2,3 +2,5 @@ secret_key = b'ineverreallyunderstoodwhatsecretkeysareforanyway'
 
 twitter_consumer_key = ""
 twitter_consumer_secret = ""
+twitter_access_token = ""
+twitter_access_token_secret = ""
